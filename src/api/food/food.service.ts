0f802443bf664b80295368d9bde07f9a1bc8d6c2
Guillermo Alexander Herrera <71296562+guialexander@users.noreply.github.com>
2023-07-12 19:31:38 -0500

    feat: gallery and food (#52)
    Co-authored-by: alex.herrera <alex.guialexander@gmail.com>

--- src/api/food/food.service.ts
+++ src/api/food/food.service.ts
@@ -17,17 +17,39 @@ export async function getFoodById(id: string) {
     return food;
 }
 
-export async function createFood(data: FoodType) {
+export async function createFood(data: FoodType, ) {
+  const restauranteId = data.restaurantsId as string;
+
   const food = await prisma.foods.create({
-    data,
+
+    data: {
+      name: data.name,
+      price: data.price,
+      image: data.image,
+      rate: data.rate,
+
+      Restaurants: {
+        connect: { id: restauranteId }
+      }
+    }
   });
     return food;
 }
 
 export async function updateFoodById(id: string, data: FoodType) {
+  const restauranteId = data.restaurantsId as string;
   const updatefood = await prisma.foods.update({
     where: { id },
-    data,
+    data: {
+      name: data.name,
+      price: data.price,
+      image: data.image,
+      rate: data.rate,
+
+      Restaurants: {
+        connect: { id: restauranteId }
+      }
+    }
 
   });
     return updatefood;
